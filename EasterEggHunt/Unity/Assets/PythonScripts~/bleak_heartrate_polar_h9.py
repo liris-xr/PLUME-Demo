2e10b1ec39ddc6711148668683d33e6411cb72e5
Javerliat Charles <charles.javerliat@gmail.com>
2024-04-12 18:42:41 +0200

    Updated recorder + added sample file

--- EasterEggHunt/Unity/Assets/PythonScripts~/bleak_heartrate_polar_h9.py
+++ EasterEggHunt/Unity/Assets/PythonScripts~/bleak_heartrate_polar_h9.py
@@ -11,6 +11,7 @@ sampling_rate = 200 # Hz
 
 uuid16_dict = {v: k for k, v in uuid16_dict.items()}
 
+ADDRESS = "E9:BD:6C:76:70:21"
 ## UUID for model number ##
 MODEL_NBR_UUID = "0000{0:x}-0000-1000-8000-00805f9b34fb".format(uuid16_dict.get("Model Number String"))
 ## UUID for manufacturer name ##
@@ -20,12 +21,13 @@ BATTERY_LEVEL_UUID = "0000{0:x}-0000-1000-8000-00805f9b34fb".format(uuid16_dict.
 HR_MEAS = "00002A37-0000-1000-8000-00805F9B34FB"
 
 def create_lsl_stream_outlet(stream_name: str):
-    info = StreamInfo(name=stream_name, type='HR', channel_count=1, nominal_srate=sampling_rate, channel_format=cf_float32, source_id='E6:C9:75:A1:6C:4E')
+    info = StreamInfo(name=stream_name, type='HR', channel_count=1, nominal_srate=sampling_rate, channel_format=cf_float32, source_id=ADDRESS)
     info.desc().append_child_value("manufacturer", "Polar")
     return StreamOutlet(info)
 
 def on_receive_data_callback(lsl_stream_outlet: StreamOutlet, hr_val: int):
     lsl_stream_outlet.push_sample([hr_val])
+    print(f"HR Value: {hr_val}")
 
 async def run(address, lsl_stream_outlet: StreamOutlet):
 
@@ -40,10 +42,11 @@ async def run(address, lsl_stream_outlet: StreamOutlet):
         print("Model Number: {0}".format("".join(map(chr, model_number))), flush=True)
         print("Manufacturer Name: {0}".format("".join(map(chr, manufacturer_name))), flush=True)
         print("Battery Level: {0}%".format(int(battery_level[0])), flush=True)
-
+        
         def hr_val_handler(sender, data):
             """Simple notification handler for Heart Rate Measurement."""
-            hr_fmt, = bitstruct.unpack("b1b1b1b1b1<", data)
+            (hr_fmt, _, _, _, _) = bitstruct.unpack("b1b1b1b1b1<", data)
+
             if hr_fmt:
                 hr_val, = struct.unpack_from("<H", data, 1)
             else:
@@ -56,7 +59,7 @@ async def run(address, lsl_stream_outlet: StreamOutlet):
             await asyncio.sleep(1.0 / sampling_rate)
 
 if __name__ == "__main__":
-    address = ("E6:C9:75:A1:6C:4E")
+    address = (ADDRESS)
     loop = asyncio.get_event_loop()
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
